9879988fd0233809c7a1ba5d2a3b23c64641896c
KS231296 <44607465+KS231296@users.noreply.github.com>
2018-11-07 17:45:53 +0100

    poprawka test z zajęć

--- Tests/edu/TestCalc.java
+++ Tests/edu/TestCalc.java
@@ -23,4 +23,40 @@ public class TestCalc {
         Assertions.assertEquals(2, sc.add(1,1));
     }
 
+    @Test
+    @DisplayName("Test / w1")
+    void divide(){
+        sc = new SimpleCalc();
+        Assertions.assertThrows(ArithmeticException.class, ()-> sc.divide(1,0));
+    }
+
+//    @Test
+//    @DisplayName("Test / w2")
+//    void divide() {
+//        sc = new SimpleCalc();
+//        Assertions.assertEquals(2, sc.divide(4, 2));
+//    }
+
+    @Test
+    @DisplayName("Test pusty obiekt")
+    void isCalcNull(){
+        sc = null;
+        Assertions.assertNull(sc , "obiekt nie jest pusty");
+    }
+
+
+    @Test
+    @DisplayName("Test porównanie referencji")
+    void compareCalc(){
+        SimpleCalc sc1 = new SimpleCalc();
+        SimpleCalc sc2 = new SimpleCalc();
+        Assertions.assertNotSame(sc1, sc2);
+    }
+
+    @Test
+    @DisplayName("Test prawda")
+    void isTrue(){
+        sc = new SimpleCalc();
+        Assertions.assertTrue(sc instanceof SimpleCalc);
+    }
 }
